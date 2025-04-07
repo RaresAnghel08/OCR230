6d8e0071e5851b8f930b786760df0bc0a74a953f
Rares08 <raresanghel2008@gmail.com>
2025-04-07 21:52:27 +0300

    Fix pdf2image conversion by specifying poppler_path parameter

--- src/ocr/ocr.py
+++ src/ocr/ocr.py
@@ -35,7 +35,7 @@ def run_processing(button_5_state, progress_bar, folder_input, folder_output, co
     pdf_files = [os.path.join(folder_input, f) for f in os.listdir(folder_input) if f.lower().endswith('.pdf')]
 
     for pdf_file in pdf_files:
-        images = pdf2image.convert_from_path(pdf_file)
+        images = pdf2image.convert_from_path(pdf_file,poppler_path=None)
         for i, image in enumerate(images):
             # Redimensionăm imaginea la dimensiunea A4
             image = image.resize((1241, 1754), Image.LANCZOS)
